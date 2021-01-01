32ea331847a00323cc5e7a8f505cd06051416c92
Kaspar Poland <kaspar78@mouco.com>
2020-12-31 19:56:42 -0700

    More error checking, only do the TOPO check every 30 minuts

--- server.js
+++ server.js
@@ -8,18 +8,18 @@ const { textMe } = require("./text-me");
 console.log("Starting server!");
 
 // Once every hour, at the 30 minute mark
-const topoBackpackJob = scheduler.scheduleJob("*/3 * * * * *", () => {
+const topoBackpackJob = scheduler.scheduleJob("* 30 * * * *", () => {
   const topoPath = path.resolve(__dirname, "./topo.py");
   const topoOptions = `--name="rover pack - classic"`;
 
   console.log("Begin checking TOPO availability:");
   exec(`python ${topoPath} ${topoOptions}`, (error, stdout, stderr) => {
+    if (stderr) return console.log(stderr);
     if (error) return console.log(error);
 
     if (stdout === "AVAILABLE\n") {
-      textMe(
-        "The Topo Designs 'Rover Pack - Classic' is available for purchase!"
-      );
+      console.log(stdout);
+      textMe("The TOPO backpack 'Rover Pack - Classic' is available!");
     } else if (stdout === "UNAVAILABLE\n") {
       console.log(
         "Backpack still unavailable",
