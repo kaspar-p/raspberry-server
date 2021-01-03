ff609b33005b2cffe3e8d2246ae9f266b861986e
Kaspar Poland <kaspar78@mouco.com>
2021-01-03 15:14:39 -0700

    Reduce size of python file

--- topo.py
+++ topo.py
@@ -2,18 +2,7 @@ from requests_html import HTMLSession
 
 product_url = "https://topodesigns.com/collections/backpacks/products/rover-pack-classic?variant=33191946944565"
 
-
-def check_availability():
-    session = HTMLSession()
-    r = session.get(product_url)
-
-    is_out_of_stock = r.html.find('p#out-of-stock-gl')
-
-    if is_out_of_stock:
-        print("UNAVAILABLE")
-    else:
-        print("AVAILABLE")
-    return
-
-
-check_availability()
+if HTMLSession().get(product_url).html.find('p#out-of-stock-gl'):
+    print("UNAVAILABLE")
+else:
+    print("AVAILABLE")
