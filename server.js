717893858392895baffda58db6bca1e24aeadfa2
Kaspar Poland <kaspar78@mouco.com>
2021-01-06 19:22:22 -0700

    Implement headless start mode

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ import _ from "lodash";
 import Bulletin from "./lib/bulletin.js";
 import createJobs from "./lib/jobs.js";
 
-// Create the table
-
 let jobs = createJobs();
 
-const bulletin = new Bulletin(jobs);
+if (displayGUI) {
+  const bulletin = new Bulletin(jobs);
+  const displayGUI = process.argv[3] === "true";
 
-_.forEach(jobs, (job, name) => {
-  job.giveParent(bulletin);
-});
-bulletin.drawFirstTime();
+  _.forEach(jobs, (job, name) => {
+    job.giveParent(bulletin);
+  });
+  bulletin.initialize();
+}
